813d0186bc14156dad5e8d3398d2057021f5ee0b
Jasko <jakejasko@gmail.com>
2020-01-09 16:35:53 -0800

    Update my_week.js

--- my_week.js
+++ my_week.js
@@ -1,4 +1,8 @@
+// Disable Logging
+console.log = function() {}
+
 const myWeekPage = "my_week";
+const loadDelay = 200;
 
 const initButtons = [
 	".left-pane-my-week",
@@ -26,29 +30,28 @@ function activeBoard(){
 	 location.href.match('(?<=boards\/)(.*?)(?=\/)')[0]; */
 	
 	const regex = "(?<=boards\/)(.*?)(?=\/)";
-	let boardID = 0;
+	let boardID;
 	try{
 		boardID = location.href.match(regex)[0];
 	}catch(error){
-		console.log("Error: No active board. Error: " + error);
+		console.log("Error: No active board. " + error);
 	}
 	
 	return boardID;
 }
 
 function folderPath(){
+	console.log(ext.libraryTest());
 	const regex = "((\\\\CCIDC1\\)|(N:\\))(.*)(?=\d)";
 	const path = $(".description-line>.text-content").text();
 	
-	if(path.indexOf(regex)){
-		return path;
-	}
+	if(path.indexOf(regex)) return path;
 	
 	return "No Valid Path";
 }
 
 function hideTasks(filterType){
-	if(filterType === "OFF"){ return; }
+	if(filterType === "OFF") return;
 	console.log("Hiding: " + filterType);
 	
 	// Hide section headers with no Tasks
@@ -57,25 +60,29 @@ function hideTasks(filterType){
 	// Hide individual task elements
 	$(".column-title > div > span").not(":contains('" + filterType + "')").each(function(i){$(this).parents().eq(5).hide();});
 
+	$(".deadline-time.done").each(function(i){$(this).parents().eq(6).hide();});
+
 	currentFilter = filterType;
 
-	$('#injected-toggle').text("[Filter by Status: " + filterType + "]");
+	$('#injected-toggle').text("Filter / " + filterType);
 	$('#injected-toggle').css("background-color", "rgb(221, 255, 221)");
 }
 
 function unhideTasks(filterType){
 	console.log("Showing all tasks.")
 	$(".column-title > div > span").not(":contains('" + filterType + "')").each(function(i){$(this).parents().eq(5).show();});
+	
+	$(".deadline-time.done").each(function(i){$(this).parents().eq(6).show();});
 
 	currentFilter = "OFF";
 
-	$('#injected-toggle').text("[Filter by Status: OFF]");
+	$('#injected-toggle').text("Filter / OFF");
 	$('#injected-toggle').css("background-color", "rgb(255, 232, 232)");
 }
 
 function inject(){
 	if($("#injected-task-filter").length === 0){
-		$(".person-filter-button-wrapper").first().append("<span id='injected-task-filter'><a id='injected-toggle' href='javascript:void(0)'>[Toggle Filter]</a></span>");
+		$(".person-filter-button-wrapper").first().append("<span id='injected-task-filter'><a id='injected-toggle' href='javascript:void(0)'>[]</a></span>");
 	}
 
 	$('#injected-toggle').click(function(){
@@ -94,7 +101,7 @@ function registerButtons(){
 	console.log("Registering buttons...");
 	
 	// jQuery multiple select -> button1, button2, etc..
-	$(initButtons.join()).click(init);
+	$(initButtons.join()).click(() => setTimeout(init, loadDelay));
 	
 	// Register collapse button to refresh filter instead of re-initializing
 	$(".section-type-container").click(function(){
@@ -109,7 +116,7 @@ function init(){
 	console.log("Checking for page load...");
 	if($(".person-filter-button-wrapper").length === 0){
 		// console.log("No person filter, re-initializing.");
-		setTimeout(init,100);
+		setTimeout(init, loadDelay);
 		return;
 	}
 	
